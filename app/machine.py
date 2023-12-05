69fb6ca3af2de27ea6ce40f34f2129046878bb63
SRobles97 <s.robles01@ufromail.cl>
2023-12-05 13:41:46 -0300

    Cambiada ruta en la que se guardará la imagen

--- app/machine.py
+++ app/machine.py
@@ -8,7 +8,7 @@ def take_picture():
     picam2.configure(camera_config)
     picam2.start()
     time.sleep(2)
-    picam2.capture_file("test.jpg")
+    picam2.capture_file("app/static/images/last_photo.jpg")
     picam2.stop()
 
 

012b584481e6059730fb7352fb0753ee48aa24d2
SRobles97 <s.robles01@ufromail.cl>
2023-12-05 21:42:57 -0300

    Incorporada funcionalidad de distancia en el hilo para sacar fotos

--- app/machine.py
+++ app/machine.py
@@ -1,7 +1,14 @@
 from picamera2 import Picamera2
+import RPi.GPIO as GPIO
 import time
 
 from app.util.files_utils import upload_image
+
+# Configurar los pines GPIO
+GPIO.setmode(GPIO.BCM)
+
+TRIG = 23  # Pin 16 como TRIG
+ECHO = 24  # Pin 18 como ECHO
 
 # Única instancia de Picamera2
 picam2 = Picamera2()
@@ -19,7 +26,47 @@ def take_picture(camera):
         camera.stop()
 
 
+def measure_distance():
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+
+    GPIO.output(TRIG, False)
+    time.sleep(2)
+
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    start_time = time.time()
+
+    while GPIO.input(ECHO) == 0:
+        start_time = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        end_time = time.time()
+
+    pulse_duration = end_time - start_time
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+
+    return distance
+
+
 def initialize_photo_process():
-    while True:
-        take_picture(picam2)
-        time.sleep(300)  # Tiempo de espera entre cada foto
+    try:
+        while True:
+            distance = measure_distance()
+            print(f"Distance: {distance} cm")
+
+            # Define la distancia prudente para tomar la foto
+            if distance < 100:  # por ejemplo, menos de 100 cm
+                take_picture(picam2)
+                # Tiempo de espera después de tomar una foto
+                time.sleep(300)
+            else:
+                # Tiempo de espera para la próxima medición de distancia
+                time.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopping distance measurement...")
+    finally:
+        GPIO.cleanup()
